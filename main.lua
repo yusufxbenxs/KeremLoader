672c194afc58ed6e38f1ec2a4a10790eb912b28a
yusufxbenxs <yigdir033@gmail.com>
2026-08-27 20:09:30 +0300

    Modify print statement and add username check
    Updated print statements to use double quotes and added a condition to load the script only for a specific username.

--- main.lua
+++ main.lua
@@ -1,9 +1,11 @@
-print('OK-KeremLoader')
+print("OK-KeremLoader")
+
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 local username = player.Name
 
-
 --- Put anything here that will run
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/yusufxbenxs/Gamebuddy/refs/heads/main/Loader.lua'))()
+if username == "doors274779" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/yusufxbenxs/Gamebuddy/refs/heads/main/Loader.lua"))()
+end
