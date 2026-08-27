25d6e45d06ca83bce43af932897c4f9633ce2ad5
yusufxbenxs <yigdir033@gmail.com>
2026-08-27 19:28:29 +0300

    Fix print statement and duplicate loadstring call

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
-Print("OK-KeremLoader")
+print('OK-KeremLoader')
+
 --- Put anything here that will run
 task.wait(8)
+
 loadstring(game:HttpGet('https://raw.githubusercontent.com/yusufxbenxs/Gamebuddy/refs/heads/main/Loader.lua'))()
