7c492d03b02aec0ef7db5295b5c9756ee8ebb969
yusufxbenxs <yigdir033@gmail.com>
2026-08-28 16:26:04 +0300

    Load external script for specific username

--- main.lua
+++ main.lua
@@ -7,5 +7,5 @@ local username = player.Name
 --- Put anything here that will run
 
 if username == "doors274779" then
-    
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/yusufxbenxs/Gamebuddy/refs/heads/main/Loader.lua'))()
 end
